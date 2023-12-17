136d388acad95dc6d9a9b1f1c2f659a3df85dc54
leishaforlinminzhi <15611664060@qq.com>
2023-12-17 15:59:30 +0800

    readme

--- lexer.py
+++ lexer.py
@@ -1,26 +1,20 @@
+import re
+
 import ply.lex as lex
 from ply.lex import TOKEN
-import sys
-from pretreatment import Pretreatment
 
-
-# token list
 tokens = (
-    # assignment
     'IDENTIFIER',
 
     'COMMENT1',
     'COMMENT2',
 
-    # types
     'FLOAT_CONSTANT',
     'INTEGER_CONSTANT',
     'STRING_CONSTANT',
     'CHAR_CONSTANT',
     'BOOL_CONSTANT',
 
-
-    # 'CHAR',
     'RIGHT_ASSIGN',
     'LEFT_ASSIGN',
     'ADD_ASSIGN',
@@ -44,12 +38,9 @@ tokens = (
     'LTE',
     'GTE',
     'BUILT_IN_FUNCTION'
-    # 'INCLUDE_ORDER'
 )
 
 reserved_keywords = {
-    # ref to https://blog.csdn.net/keke193991/article/details/17566937
-    # ref to https://blog.csdn.net/qq_41687938/article/details/119349135
     'asm': 'ASM',
     'auto': 'AUTO',
     'bool': 'BOOL',
@@ -113,6 +104,10 @@ reserved_keywords = {
     'while': 'WHILE',
 }
 
+# 将预定义的关键字列表中的所有值添加到 tokens 列表中
+# reserved_keywords.values() 返回一个包含所有值的可迭代对象
+# 使用 tuple() 函数将其转换为元组，然后通过 + 运算符将其与 tokens 列表连接起来
+# 这样做是为了确保词法分析器能够识别并处理这些关键字。
 tokens = tokens + tuple(reserved_keywords.values())
 
 
@@ -146,22 +141,29 @@ t_ignore  = ' \t\v\f'
 literals = "+-*/%|&~^<>=!?()[]{}.,;:\\\'\""
 
 
+# 匹配不同形式的整数常量，包括十进制、十六进制
+# 前一部分匹配数字，后一部分匹配代表四种可能的后缀组合，包括 ul、lu、u 或 l
 def t_INTEGER_CONSTANT(t):
     r'(((((0x)|(0X))[0-9a-fA-F]+)|(\d+))([uU][lL]|[lL][uU]|[uU]|[lL])?)'
     return t
 
+# 匹配小数点表示的浮点数和匹配科学计数法表示的浮点数
+# 小数点表示中支持指数表示
 def t_FLOAT_CONSTANT(t):
     r'((\d+)(\.\d+)(e(\+|-)?(\d+))? | (\d+)e(\+|-)?(\d+))([lL]|[fF])?'
     return t
 
+# 匹配以单引号包围的字符常量
 def t_CHAR_CONSTANT(t):
     r'(L)?\'([^\\\n]|(\\(.|\n)))*?\''
     return t
 
+# bool类型
 def t_BOOL_CONSTANT(t):
     r'(true|false)'
     return t
 
+# 被“”修饰的字符串
 def t_STRING_CONSTANT(t):
     r'"(\\.|[^\\"])*"'
     return t
@@ -177,42 +179,47 @@ def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# error handle
+
+# 错误处理
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-# comment
-def t_COMMENT2(t):
-    r'(//.*?(\n|$))'
-    pass
-
+# \\类型的注释
 def t_COMMENT1(t):
-    r'(/\*(.|\n)*?\*/)'
-    pass
+    r'(//.*?(\n|$))'
+    return t
 
-# create lexer
+# /**/类型的注释
+def t_COMMENT2(t):
+    r'(/\*(.|\n)*?\*/)'
+    t.value = t.value.replace(' ','')
+    t.value = t.value.replace('\n',' ')
+    return t
+
 lexer = lex.lex()
 
 
-
 if __name__ == '__main__':
+    import sys
+    from preprocess import preprocess
 
-    if len(sys.argv) > 1:  # specify file
+    if len(sys.argv) > 1:  
         try:
-            pretreatmenter=Pretreatment()
-            file_data, ok=pretreatmenter.Pretreatment(sys.argv[1])
-            
+            file_data, ok = preprocess(sys.argv[1])
             if ok is not True:
-                print('Pretreatment error with file:', file_data)
+                print('preprocess error:', file_data)
             else:
                 lexer.input(file_data)
-                while True:
+                # 从左向右匹配
+                while 1:
                     token = lexer.token()
                     if not token:
-                        break  # not input
+                        break  
                     print(token)
         except Exception as e:
             print(e)
     else:
         print("please input c++ file path")
+
+
